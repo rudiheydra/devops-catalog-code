801acf6662f76942b0de3ea3609adcba67dc0f17
Rudi Heydra <rudi.heydra@gmail.com>
2022-08-05 12:20:14 +1000

    updated backend.tf to generate random s3 bucket names

--- terraform-eks/files/backend.tf
+++ terraform-eks/files/backend.tf
@@ -1,6 +1,13 @@
+
+resource "random_string" "random" {
+  length = 6
+  special = false
+  upper = false
+}
+
 terraform {
   backend "s3" {
-    bucket = "devops-catalog"
+    bucket = "devops-catalog-${random_string.random.result}"
     key    = "terraform/state"
   }
 }
